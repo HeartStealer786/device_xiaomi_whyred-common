818f68e785e238763aba6f5204838093020d220e
HeartStealer786 <beingme639@gmail.com>
2020-04-05 16:06:54 +0530

    whyred: overlay: Give light sensor some time to warm up
    Signed-off-by: HeartStealer786 <beingme639@gmail.com>

--- device.mk
+++ device.mk
@@ -63,6 +63,10 @@ PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
     android.hardware.keymaster@3.0-service
 
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.light@2.0-service.xiaomi_whyred
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(DEVICE_PATH)/overlay \
